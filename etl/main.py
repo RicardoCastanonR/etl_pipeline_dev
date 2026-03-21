2fac7cbdfecc9ff8b98ed9fcf5913ebdf83b5c50
Ricardo CR <ricardo.castanon44@gmail.com>
2026-03-20 18:07:39 -0700

    Basic filter operation

--- etl/main.py
+++ etl/main.py
@@ -1,14 +1,32 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import upper, col
 
 spark = SparkSession.builder \
     .appName("TestApp") \
     .config("spark.jars", "/opt/spark/") \
     .getOrCreate()
 
-data = [("Ricardo", 27), ("Ana", 25)]
-df = spark.createDataFrame(data, ["name", "age"])
+"""
+ data = [("Ricardo", 27), ("Ana", 25)]
+ df = spark.createDataFrame(data, ["name", "age"])
+ 
+ df.show()
+ 
+ spark.stop()
+"""
 
-df.show()
+data = [
+    (1, "Ana", 23, "US"),
+    (2, "Luis", 17, "MX"),
+    (3, "John", 30, "US"),
+    (4, "Maria", 15, "MX")
+]
 
-spark.stop()
+df = spark.createDataFrame(data, ["id", "name", "age", "country"])
+
+filtered_df = df \
+    .filter(df.age >= 18) \
+    .withColumn("name", upper(col("name")))
+
+filtered_df.show()
